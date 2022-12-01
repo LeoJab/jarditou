91c5972fa556ed3544927f7bd12e2335da0234c4
LeoJab <leojabelin@laposte.net>
2022-12-01 16:53:32 +0100

    jarditou 1/12

--- ASSET/JS/exercice1.js
+++ ASSET/JS/exercice1.js
@@ -14,6 +14,8 @@ var random = Math.floor(Math.random() * (max - min) + min);
   }
  }
 
+ //---------------------------------------------------------
+
 function enregistrer()
 {
     var reponse1 = window.prompt("Saisissez votre nom")
@@ -21,6 +23,8 @@ function enregistrer()
     var reponse3 = window.confirm("Etes-vous un homme ?")
     window.alert("Bonjour Monieur" + " " + reponse1 + " " + reponse2 + " " + "Bienvenue sur notre site web !")
 }
+
+//---------------------------------------------------------
 
 function verification()
 {
@@ -32,12 +36,95 @@ function verification()
     }
 }
 
+//---------------------------------------------------------
+
 function CalculAge()
 {
     var today = new Date();
     var ddn = document.getElementById("ddn").value;
+    
     var an=ddn.substr(6,4);
     var mois=ddn.substr(3,2);
     var jour=ddn.substr(0,2);
     
+    var dateNaissance = new Date(an + "-" + mois + "-" + jour);
+    
+    var age = today.getFullYear() - dateNaissance.getFullYear();
+    var m = today.getMonth() - dateNaissance.getMonth()
+    if (m < 0 || (m === 0 && today.getDate() < dateNaissance.getDate())) {
+      age = age - 1;
+    }
+
+    document.getElementById("age").innerHTML= "Votre Age : " +  age;
 }
+
+//---------------------------------------------------------
+
+function CalculPlus()
+{
+  var n1 = document.getElementById("nombre1").value;
+  var n2 = document.getElementById("nombre2").value;
+  var resultatplus = parseInt(n1) + parseInt(n2);
+
+  document.getElementById("resultat").value = resultatplus;
+}
+
+function CalculMoins()
+{
+  var n1 = document.getElementById("nombre1").value;
+  var n2 = document.getElementById("nombre2").value;
+  var resultatmoins = parseInt(n1) - parseInt(n2);
+
+  document.getElementById("resultat").value = resultatmoins;
+}
+
+function CalculMulti()
+{
+  var n1 = document.getElementById("nombre1").value;
+  var n2 = document.getElementById("nombre2").value;
+  var resultatmulti = parseInt(n1) * parseInt(n2);
+
+  document.getElementById("resultat").value = resultatmulti;
+}
+
+function CalculDivi()
+{
+  var n1 = document.getElementById("nombre1").value;
+  var n2 = document.getElementById("nombre2").value;
+  var resultatdivi = parseInt(n1) / parseInt(n2);
+  if (parseInt(n2) === 0){
+    document.getElementById("resultat").value = parseInt(n2) / parseInt(n1);
+  } else{
+    document.getElementById("resultat").value = resultatdivi;
+  }
+}
+
+//---------------------------------------------------------
+
+function calcul()
+{
+  var operateur = document.getElementsByClassName("operateur");
+  var n1 = document.getElementById("nombre3").value;
+  var n2 = document.getElementById("nombre4").value;
+  var resultat;
+
+  switch(n1, n2, operateur){
+    case '+':
+      var resultat = parseInt(n1) + parseInt(n2);
+      document.getElementById("resultat2").value = resultat;
+      break;
+    case '-':
+      var resultat = parseInt(n1) - parseInt(n2);
+      document.getElementById("resultat2").value = resultat;
+      break;
+    case '*':
+      var resultat = parseInt(n1) * parseInt(n2);
+      document.getElementById("resultat2").value = resultat;
+      break;
+    case '/':
+      var resultat = parseInt(n1) / parseInt(n2);
+      document.getElementById("resultat2").value = resultat;
+      break;
+  }
+}
+
